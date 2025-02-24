1de5720aa1e4338918e18da631d447a220810aa7
Emanuele-n <manunicotra97@gmail.com>
2025-02-24 16:27:16 +1100

    utils

---
+++ take_photo.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+
+
+def take_photo(cam_index):
+    save_dir = "./data"
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    cap = cv2.VideoCapture(cam_index)
+    if not cap.isOpened():
+        print("Error: Cannot access the camera")
+        return
+
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame")
+        cap.release()
+        return
+
+    photo_path = os.path.join(save_dir, "photo.png")
+    cv2.imwrite(photo_path, frame)
+    print(f"Photo saved at {photo_path}")
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    cam_index = 4
+    take_photo(cam_index)
